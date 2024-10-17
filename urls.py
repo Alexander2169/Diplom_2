a5258f7ae2a70911a248225ebd7442ebf6c79a11
Alexander Cheremisov <dadic2169@mail.ru>
2024-10-17 15:00:44 +0300

    modified urls.py

--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 BASE_URL = "https://stellarburgers.nomoreparties.site/api"
 
-AUTH_REGISTER_URL = f"{BASE_URL}/auth/register"
-AUTH_LOGIN_URL = f"{BASE_URL}/auth/login"
-AUTH_USER_URL = f"{BASE_URL}/auth/user"
-ORDER_URL = f"{BASE_URL}/orders"
+AUTH_REGISTER_URL = f"{BASE_URL}/auth/register" # для создания пользователя
+AUTH_LOGIN_URL = f"{BASE_URL}/auth/login" # для авторизации
+AUTH_USER_URL = f"{BASE_URL}/auth/user" # для получения информации о пользователе
+ORDER_URL = f"{BASE_URL}/orders" # для создания заказа
