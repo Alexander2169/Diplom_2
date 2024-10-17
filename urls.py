9548a3a6b181d51ff94f35dc1c44281785006c24
Alexander Cheremisov <dadic2169@mail.ru>
2024-10-17 22:42:51 +0300

    delete files tests,urls,tokens,conftests

--- urls.py
+++
@@ -1,6 +0,0 @@
-BASE_URL = "https://stellarburgers.nomoreparties.site/api"
-
-AUTH_REGISTER_URL = f"{BASE_URL}/auth/register" # для создания пользователя
-AUTH_LOGIN_URL = f"{BASE_URL}/auth/login" # для авторизации
-AUTH_USER_URL = f"{BASE_URL}/auth/user" # для получения информации о пользователе
-ORDER_URL = f"{BASE_URL}/orders" # для создания заказа
