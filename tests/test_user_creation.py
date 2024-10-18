d4ea6d11925490727dce879799fee17d605644be
Alexander Cheremisov <dadic2169@mail.ru>
2024-10-18 03:39:49 +0300

    modifieds: data.py/test_user_creation.py/test_user_login.py

--- tests/test_user_creation.py
+++ tests/test_user_creation.py
@@ -1,8 +1,8 @@
 from data import *
 
-class UserTestsCreation(UserData):
+class UserTestsCreation(UserData): # Создание пользователя
 
-    def test_check_create_user(self):
+    def test_check_create_user(self): # Проверяем создание пользователя
         response = self.user_client.post_create_new_user(self.user)
         assert response.status_code == 200
         assert response.json().get("success") is True
@@ -12,37 +12,37 @@ class UserTestsCreation(UserData):
         assert response.json().get("user").get("name") is not None
         self.token = response.json().get("accessToken")  # Сохраним токен для удаления
 
-    def test_check_registered_user(self):
+    def test_check_registered_user(self): # Проверяем зарегистрированного пользователя
         self.user_client.post_create_new_user(self.user)
         response = self.user_client.post_create_new_user(self.user)
         assert response.status_code == 403
         assert response.json().get("success") is False
         assert response.json().get("message") == "User already exists"
 
-    def test_create_user_without_name(self):
+    def test_create_user_without_name(self): # Проверяем созданного пользователя без имени
         self.user.email = self.email
         self.user.password = self.password
         response = self.user_client.post_create_new_user(self.user)
         self.user_client.check_failed_response_auth_register(response)
 
-    def test_create_user_without_email(self):
+    def test_create_user_without_email(self): # Проверяем созданного пользователя без эл. почты
         self.user.name = self.name
         self.user.password = self.password
         response = self.user_client.post_create_new_user(self.user)
         self.user_client.check_failed_response_auth_register(response)
 
-    def test_create_user_without_password(self):
+    def test_create_user_without_password(self): # Проверяем созданного пользователя без пароля
         self.user.email = self.email
         self.user.name = self.name
         response = self.user_client.post_create_new_user(self.user)
         self.user_client.check_failed_response_auth_register(response)
 
-    def test_create_user_without_name_and_email(self):
+    def test_create_user_without_name_and_email(self): # Проверяем созданного пользователя без имени и эл. почты
         self.user.password = self.password
         response = self.user_client.post_create_new_user(self.user)
         self.user_client.check_failed_response_auth_register(response)
 
-    def test_create_user_without_name_and_password(self):
+    def test_create_user_without_name_and_password(self): # Проверяем созданного пользователя без имени и пароля
         self.user.email = self.email
         response = self.user_client.post_create_new_user(self.user)
         self.user_client.check_failed_response_auth_register(response)
--- tests/test_user_login.py
+++ tests/test_user_login.py
@@ -1,12 +1,12 @@
 from data import *
 
-class UserTestsLogin(UserData):
-    def test_login_existing_user(self):
+class UserTestsLogin(UserData): # Логин пользователя
+    def test_login_existing_user(self): # Проверяем логин под существующим пользователем
         response = self.user_client.post_login_user(self.user.email, self.user.password)
         assert response.status_code == 200
         assert response.json().get("accessToken") is not None
 
-    def test_login_with_invalid_credentials(self):
+    def test_login_with_invalid_credentials(self): # Проверяем логин с неверным логином и паролем
         response = self.user_client.post_login_user("invalid@mail.com", "wrongpassword")
         assert response.status_code == 401
         assert response.json().get("success") is False
