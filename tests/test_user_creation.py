5bf29ee6760f2a4af8a1418a3a13e2efe3e90b1c
Alexander Cheremisov <dadic2169@mail.ru>
2024-10-18 12:19:21 +0300

    modifieds files: utils.py/test_user_login.py/test_user_creation.py

--- tests/test_user_creation.py
+++ tests/test_user_creation.py
@@ -1,8 +1,9 @@
 from utils import *
+import allure
 
 class UserTestsCreation(UserData): # Создание пользователя
-
-    def test_check_create_user(self): # Проверяем создание пользователя
+    @allure.title('Проверяем создание пользователя')
+    def test_check_create_user(self):
         response = self.user_client.post_create_new_user(self.user)
         assert response.status_code == 200
         assert response.json().get("success") is True
@@ -11,41 +12,40 @@ class UserTestsCreation(UserData): # Создание пользователя
         assert response.json().get("user").get("email") is not None
         assert response.json().get("user").get("name") is not None
         self.token = response.json().get("accessToken")  # Сохраним токен для удаления
-
-    def test_check_registered_user(self): # Проверяем зарегистрированного пользователя
+    @allure.title('Проверяем зарегистрированного пользователя')
+    def test_check_registered_user(self):
         self.user_client.post_create_new_user(self.user)
         response = self.user_client.post_create_new_user(self.user)
         assert response.status_code == 403
         assert response.json().get("success") is False
         assert response.json().get("message") == "User already exists"
-
-    def test_create_user_without_name(self): # Проверяем созданного пользователя без имени
+    @allure.title('Проверяем созданного пользователя без имени')
+    def test_create_user_without_name(self):
         self.user.email = self.email
         self.user.password = self.password
         response = self.user_client.post_create_new_user(self.user)
         self.user_client.check_failed_response_auth_register(response)
-
-    def test_create_user_without_email(self): # Проверяем созданного пользователя без эл. почты
+    @allure.title('Проверяем созданного пользователя без электронной почты')
+    def test_create_user_without_email(self): #
         self.user.name = self.name
         self.user.password = self.password
         response = self.user_client.post_create_new_user(self.user)
         self.user_client.check_failed_response_auth_register(response)
-
-    def test_create_user_without_password(self): # Проверяем созданного пользователя без пароля
+    @allure.title('Проверяем созданного пользователя без пароля')
+    def test_create_user_without_password(self):
         self.user.email = self.email
         self.user.name = self.name
         response = self.user_client.post_create_new_user(self.user)
         self.user_client.check_failed_response_auth_register(response)
-
-    def test_create_user_without_name_and_email(self): # Проверяем созданного пользователя без имени и эл. почты
+    @allure.title('Проверяем созданного пользователя без имени и электронной почты')
+    def test_create_user_without_name_and_email(self):
         self.user.password = self.password
         response = self.user_client.post_create_new_user(self.user)
         self.user_client.check_failed_response_auth_register(response)
-
-    def test_create_user_without_name_and_password(self): # Проверяем созданного пользователя без имени и пароля
+    @allure.title('Проверяем созданного пользователя без имени и пароля')
+    def test_create_user_without_name_and_password(self):
         self.user.email = self.email
         response = self.user_client.post_create_new_user(self.user)
         self.user_client.check_failed_response_auth_register(response)
 
-if __name__ == "__main__":
-    unittest.main()
+
--- tests/test_user_login.py
+++ tests/test_user_login.py
@@ -1,12 +1,14 @@
 from utils import *
+import allure
 
-class UserTestsLogin(UserData): # Логин пользователя
-    def test_login_existing_user(self): # Проверяем логин под существующим пользователем
+class UserTestsLogin(UserData): # Проверяем логин пользователя
+    @allure.title('Проверяем логин под существующим пользователем')
+    def test_login_existing_user(self):
         response = self.user_client.post_login_user(self.user.email, self.user.password)
         assert response.status_code == 200
         assert response.json().get("accessToken") is not None
-
-    def test_login_with_invalid_credentials(self): # Проверяем логин с неверным логином и паролем
+    @allure.title('Проверяем логин с неверным логином и паролем')
+    def test_login_with_invalid_credentials(self):
         response = self.user_client.post_login_user("invalid@mail.com", "wrongpassword")
         assert response.status_code == 401
         assert response.json().get("success") is False
