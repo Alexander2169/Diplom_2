05a7f7d4b1824a58a4876e22444f0198f57a5e53
Alexander Cheremisov <dadic2169@mail.ru>
2024-10-22 20:38:52 +0300

    modified test_order_creation.py

--- tests/test_order_creation.py
+++ tests/test_order_creation.py
@@ -1,9 +1,10 @@
+import allure
 from helpers import *
 from conftest import *
 from data import INGREDIENTS
 
-class TestOrderCreation:
-
+class TestOrderCreation: # Тесты для создания заказов
+    @allure.title('Проверка создания заказа с авторизацией')
     def test_create_order_with_auth(self, clean_user, base_url):
         response = login_user(clean_user)
         token = response.json()["accessToken"]
@@ -11,20 +12,20 @@ class TestOrderCreation:
         response = create_order(ingredient_ids, token)
         assert response.status_code == 200
         assert response.json()["success"] is True
-
+    @allure.title('Проверка создания заказа без авторизации')
     def test_create_order_without_auth(self, base_url):
         ingredient_ids = INGREDIENTS.INGREDIENT_IDS
         response = create_order(ingredient_ids, "")
         assert response.status_code == 200
         assert response.json()["success"] is True
-
-    def test_create_order_without_ingredients(self, clean_user, base_url):
+    @allure.title('Проверка создания заказа без ингредиентов')
+    def test_create_order_without_ingredients(self, clean_user, base_url): #
         response = login_user(clean_user)
         token = response.json()["accessToken"]
         response = create_order([], token)
         assert response.status_code == 400
         assert response.json()["success"] is False
-
+    @allure.title('Проверка создания заказа с неверным ID ингредиента')
     def test_create_order_with_wrong_ingredient_hash(self, clean_user, base_url):
         response = login_user(clean_user)
         token = response.json()["accessToken"]
