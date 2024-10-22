79ac7b7def2d86f87b8801cce82a67f47699cc24
Alexander Cheremisov <dadic2169@mail.ru>
2024-10-22 20:51:46 +0300

    modified test_user_login.py

--- tests/test_user_login.py
+++ tests/test_user_login.py
@@ -1,12 +1,14 @@
+import allure
 from helpers import login_user
 from conftest import *
 
-class TestUserLogin:
+class TestUserLogin: # Тесты для логина пользователей
+    @allure.title('Проверка логина под существующим пользователем')
     def test_login_existing_user(self, clean_user, base_url):
         response = login_user(clean_user)
         assert response.status_code == 200
         assert response.json()["success"] is True
-
+    @allure.title('Проверка логина с неверными данными')
     def test_login_with_wrong_credentials(self, base_url):
         user = User.generate_random_user()
         response = login_user(user)
