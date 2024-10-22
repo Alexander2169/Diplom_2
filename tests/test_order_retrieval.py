12542436129a9afa5df1c75a2cd08fab97067eab
Alexander Cheremisov <dadic2169@mail.ru>
2024-10-22 20:43:02 +0300

    modified test_order_retrieval.py

--- tests/test_order_retrieval.py
+++ tests/test_order_retrieval.py
@@ -1,14 +1,16 @@
+import allure
 from helpers import *
 from conftest import *
 
-class TestOrderRetrieval:
+class TestOrderRetrieval: # Тесты для получения заказов
+    @allure.title('Проверка получения заказов авторизованным пользователем')
     def test_get_orders_as_authorized_user(self, clean_user, base_url):
         response = login_user(clean_user)
         token = response.json()["accessToken"]
         response = get_orders(token)
         assert response.status_code == 200
         assert response.json()["success"] is True
-
+    @allure.title('Проверка получения заказов неавторизованным пользователем')
     def test_get_orders_as_unauthorized_user(self, base_url):
         response = get_orders("")
         assert response.status_code == 401
